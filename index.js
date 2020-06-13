09072834f244af19c81ac4b6e15be7cfd1877144
Alessandro Fragnani <alefragnani@hotmail.com>
2020-06-13 11:37:53 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Alessandro Fragnani. All rights reserved.
+*  Licensed under the MIT License. See License.md in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+'use strict'
+module.exports = {
+	parser: require.resolve('@typescript-eslint/parser'),
+	parserOptions: {
+		sourceType: 'module'
+	},
+	plugins: [
+		'@typescript-eslint',
+	],
+	settings: {
+		'import/resolver': {
+			node: {
+				extensions: [
+					'.ts'
+				]
+			}
+		},
+		'import/parsers': {
+			'@typescript-eslint/parser': [
+				'.ts'
+			]
+		}
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+    rules: {
+		'@typescript-eslint/explicit-module-boundary-types': 0,
+        '@typescript-eslint/no-unused-vars': 1,
+        '@typescript-eslint/no-explicit-any': 1,
+		'@typescript-eslint/no-non-null-assertion': 1,
+		"eqeqeq": 2
+    }
+};
